514ab3138a311b9a64d3430effc85aefde867f00
Furkan Elkatmış <elkatmisfurkan65@gmail.com>
2024-01-07 16:09:53 +0300

    a
    a

--- course.m
+++ course.m
@@ -148,3 +148,34 @@ classdef course
     end
 end
 
+% Course class definition
+classdef course
+    properties
+        Name (1,1) string
+        Level (1,1) string
+        Waypoints (:,1) double
+    end
+
+    methods
+        function c = course(name,lvl,waypts)
+            c.Name = name;
+            c.Level = lvl;
+            c.Waypoints = waypts;
+        end
+
+        function ok = checkWaypoint(c,id,wayptidx)
+            % Get the index of the previous waypoint found
+            [~,prevwpidx] = max(id.Timestamps);
+            % Current waypoint should be the next one
+            ok = (wayptidx == (1+prevwpidx));
+        end
+
+        function disp(c)
+            str = c.Level + " course '" + c.Name + "' with " + ...
+                numel(c.Waypoints) + " waypoints:";
+            disp(str)
+            disp(c.Waypoints)
+        end
+    end
+
+end
